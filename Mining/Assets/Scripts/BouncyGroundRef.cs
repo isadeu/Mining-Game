909660c0bebce5e0efddc65f0184c74149c5abb9
Isa Deu <ipd220@nyu.edu>
2017-01-30 10:57:26 -0500

    Added mining game to repo
    has basic movement and bounce

---
+++ Mining/Assets/Scripts/BouncyGroundRef.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncyGroundRef : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter2D (Collision2D thingthatHitMe)
+	{
+		thingthatHitMe.gameObject.GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0,9f), ForceMode2D.Impulse);//when objects hit this object they move side to side 2 spaces and uo 6
+	}
+}
---
+++ Mining/Assets/Scripts/ChMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			transform.position += new Vector3 (.1f, 0, 0);//RIGHT position is stored on a vector3
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			transform.position -= new Vector3 (.1f, 0, 0);//LEFT ^but instead of minus bc its moving right not left
+		}
+		if (Input.GetKeyDown (KeyCode.UpArrow))
+			transform.position += new Vector3 (0, 2f, 0); //jump
+	}
+}
